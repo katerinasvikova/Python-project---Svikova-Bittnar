2af0c0fb96fa1ab59cf8f0ce6b5f92a53e044378
PavelBittnar <166150446+PavelBittnar@users.noreply.github.com>
2025-02-06 19:39:37 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,16 @@
 import pandas as pd
-from Functions.Fetch import fetch_estates_data
+from Functions.Fetch import RealEstateFetcher
 from Functions.Preprocess import preprocess_data
 
 df = pd.read_csv('real_estate_prague.csv')
 
 df = preprocess_data(df)
+
+# Initialize the RealEstateFetcher class
+real_estate_fetcher = RealEstateFetcher()
+
+# Fetch new data and save it to the file (this will handle checking for new ads and saving)
+new_data = real_estate_fetcher.fetch_and_save_data()
+
+# Optionally, display the first few rows of the fetched data
+print(new_data.head())
