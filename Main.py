8e9e76db5ebd431535ea9c0f13ef82f96b9c9cf4
PavelBittnar <166150446+PavelBittnar@users.noreply.github.com>
2025-02-06 21:37:42 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,17 @@
-from Functions.Fetch import fetch_estates_data
+import joblib
+import pandas as pd
+
+from Functions.Fetch import RealEstateFetcher
 from Functions.Preprocess import preprocess_data
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
 
 # Initialize the RealEstateFetcher class
 real_estate_fetcher = RealEstateFetcher()
 
 # Fetch new data and save it to the file (this will handle checking for new ads and saving)
-new_data = real_estate_fetcher.fetch_and_save_data()
+new_data = real_estate_fetcher.fetch_and_exctract_data()
 
 predict_price = new_data[new_data['Is New'] == True]
 
@@ -15,7 +20,7 @@ if predict_price.empty:
     print("No new data to predict.")
 else:
     # Preporocess the data
-    predict_price = preprocess(predict_price)
+    predict_price = preprocess_data(predict_price)
     
     X = predict_price[['Size_m2', 'Latitude', 'Longitude', 'Flat Type']] 
     y = predict_price['Price (CZK)']
@@ -31,6 +36,25 @@ else:
     # Filter only observations where the predicted price is lower than the actual price
     price_drops = predict_price[predict_price['Predicted Price'] < predict_price['Price (CZK)']]
 
-    # Return only those observations
-    import ace_tools as tools  # Use ace_tools to display DataFrame
-    tools.display_dataframe_to_user(name="Predicted Price Drops", dataframe=price_drops)
+    # Retraining the random forest
+    X_retrain = predict_price[['Size_m2', 'Latitude', 'Longitude', 'Flat Type']]
+    y_retrain = predict_price['Price (CZK)']
+
+    X_train, X_test, y_train, y_test = train_test_split(X_retrain, y_retrain, test_size = 0.2, random_state = 42)
+    
+    new_model = RandomForestRegressor(n_estimators = 1000,
+                                      random_state = 42,
+                                      max_depth = 10,
+                                      min_samples_split = 5
+                                     )
+    new_model.fit(X_train, y_train)
+
+    predictions = new_model.predict(X_test)
+
+    joblib.dump(new_model, 'random_forest_model_compressed.pkl', compress=3)
+
+    
+
+    
+
+    
