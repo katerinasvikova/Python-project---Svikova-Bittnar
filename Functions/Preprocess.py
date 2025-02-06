dd362bb6cdb6916ee868dcfef2346ec4e132d1b9
Kateřina Švíková <73632303@fsv.cuni.cz>
2025-02-06 19:11:54 +0100

    Fix

--- Functions/Preprocess.py
+++ Functions/Preprocess.py
@@ -1,32 +1,21 @@
 import pandas as pd  
 import re
 
-# Remove zero-price flats
-def remove_zero_price_flat(data):
-  if (data['Price (CZK)'] == 0).any():
-    data = data[data['Price (CZK)'] != 0]
-    print("Zero-price flats have been removed.")
-  else:
-    print("No zero-price flats found.")
-    data = data.reset_index(drop=True)
-    
-    return data
-
-# Check for missing values
-def check_missing_values(data):
-  for column in data.columns:
-    missing_values = data[column].isna().sum()
-    
-    if missing_values > 0:
-      print(f"Column '{column}' has {missing_values} missing values so we remove them.")
-    
-    data = data.dropna().reset_index(drop=True)
-  return data
-
 def preprocess_data(data):
     # Extract the size of the flat from the 'Name' column
     data['Size_m2'] = data['Name'].apply(lambda x: int(re.findall(r'(\d+)\s*m²', x)[0]) if re.findall(r'(\d+)\s*m²', x) else None)
-  
+
+    # Remove zero-price flats
+    def remove_zero_price_flat(data):
+        if (data['Price (CZK)'] == 0).any():
+            data = data[data['Price (CZK)'] != 0]
+            print("Zero-price flats have been removed.")
+        else:
+            print("No zero-price flats found.")
+        data = data.reset_index(drop=True)
+        
+        return data
+
     data = remove_zero_price_flat(data)
     
     # Split the 'Location' into District and Municipal Part
@@ -42,6 +31,16 @@ def preprocess_data(data):
 
     data = data.drop('Name', axis=1) #delete 'Name' column
     data = data.drop('Location', axis=1) #delete 'Location' column
+
+    # Check for missing values
+    def check_missing_values(data):
+        for column in data.columns:
+            missing_values = data[column].isna().sum()
+            if missing_values > 0:
+                print(f"Column '{column}' has {missing_values} missing values so we remove them.")
+        #Drop the missing values
+        data = data.dropna().reset_index(drop=True)
+        return data
     
     data = check_missing_values(data)
 
--- Main.py
+++ Main.py
@@ -1,2 +1,7 @@
+import pandas as pd
 from Functions.Fetch import fetch_estates_data
 from Functions.Preprocess import preprocess_data
+
+df = pd.read_csv('real_estate_prague.csv')
+
+df = preprocess_data(df)
