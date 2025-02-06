172687ce125d294675b63cb7f9153d27e2b01420
Kateřina Švíková <73632303@fsv.cuni.cz>
2025-02-06 19:57:02 +0100

    fetching dataset

--- Main.py
+++ Main.py
@@ -2,15 +2,11 @@ import pandas as pd
 from Functions.Fetch import RealEstateFetcher
 from Functions.Preprocess import preprocess_data
 
-df = pd.read_csv('real_estate_prague.csv')
-
-df = preprocess_data(df)
-
 # Initialize the RealEstateFetcher class
 real_estate_fetcher = RealEstateFetcher()
 
 # Fetch new data and save it to the file (this will handle checking for new ads and saving)
-new_data = real_estate_fetcher.fetch_and_save_data()
+new_data = real_estate_fetcher.fetch_and_exctract_data()
 
 # Optionally, display the first few rows of the fetched data
 print(new_data.head())
