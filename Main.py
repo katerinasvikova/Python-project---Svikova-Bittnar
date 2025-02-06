dd94a92111c377dd24cf3747f659fe053b0c518d
PavelBittnar <166150446+PavelBittnar@users.noreply.github.com>
2025-02-06 20:17:30 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,24 @@
-import pandas as pd
-from Functions.Fetch import RealEstateFetcher
+from Functions.Fetch import fetch_estates_data
 from Functions.Preprocess import preprocess_data
+from sklearn.ensemble import RandomForestRegressor
 
 # Initialize the RealEstateFetcher class
 real_estate_fetcher = RealEstateFetcher()
 
 # Fetch new data and save it to the file (this will handle checking for new ads and saving)
-new_data = real_estate_fetcher.fetch_and_exctract_data()
+new_data = real_estate_fetcher.fetch_and_save_data()
 
-# Optionally, display the first few rows of the fetched data
-print(new_data.head())
+predict_price = new_data[new_data['Is New'] == True]
+
+# Check if the new dataset is empty
+if predict_price.empty:
+    print("No new data to predict.")
+else:
+    # Preporocess the data
+    predict_price = preprocess(predict_price)
+    
+    X = predict_price[['Size_m2', 'Latitude', 'Longitude', 'Flat Type']] 
+    y = predict_price['Price (CZK)']
+    
+    # Load model 
+    model = joblib.load('random_forest_model_compressed.pkl')
