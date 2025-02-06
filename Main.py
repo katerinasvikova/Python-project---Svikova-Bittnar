57d9184afb432ed278f1509e714896a42005c347
PavelBittnar <166150446+PavelBittnar@users.noreply.github.com>
2025-02-06 20:15:31 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,13 +10,15 @@ new_data = real_estate_fetcher.fetch_and_save_data()
 
 predict_price = new_data[new_data['Is New'] == True]
 
-
-# Preporocess the data
-predict_price = preprocess(predict_price)
-
-
-X = predict_price[['Size_m2', 'Latitude', 'Longitude', 'Flat Type']] 
-y = predict_price['Price (CZK)']
-
-# Load model 
-model = joblib.load('linear_regression_model.pkl')
+# Check if the new dataset is empty
+if predict_price.empty:
+    print("No new data to predict.")
+else:
+    # Preporocess the data
+    predict_price = preprocess(predict_price)
+    
+    X = predict_price[['Size_m2', 'Latitude', 'Longitude', 'Flat Type']] 
+    y = predict_price['Price (CZK)']
+    
+    # Load model 
+    model = joblib.load('random_forest_model_compressed.pkl')
